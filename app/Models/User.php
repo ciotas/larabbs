5627cec12387613cb3510ec1e7c2dc2b1ef4480f
tongmeng <linzikristy@qq.com>
2020-01-07 06:30:04 +0000

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
