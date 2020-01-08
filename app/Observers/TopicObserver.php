4ffcdabf48d82d28d26e59858ea836d02ece0935
tongmeng <linzikristy@qq.com>
2020-01-08 06:03:40 +0000

    用户可以编辑话题

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -9,9 +9,12 @@ use App\Models\Topic;
 
 class TopicObserver
 {
-
+    // creating, created, updating, updated, saving,
+    // saved,  deleting, deleted, restoring, restored
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 
