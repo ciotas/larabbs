15be2a4c1d2c54a12aac760c1d020a6db62b87ac
tongmeng <linzikristy@qq.com>
2020-01-16 15:21:26 +0000

    edit config/adminstrator/topic.php

--- config/administrator/topics.php
+++ config/administrator/topics.php
@@ -16,10 +16,7 @@ return [
             'title'    => '话题',
             'sortable' => false,
             'output'   => function ($value, $model) {
-                return
-//                    '<div style="max-width:260px">' . model_link($value, $model) . '</div>'
-                    '<div style="max-width:260px">' . model_admin_link(e($model->title), $model) . '</div>'
-                    ;
+                return '<div style="max-width:260px">' . model_link($value, $model) . '</div>';
             },
         ],
         'user' => [
