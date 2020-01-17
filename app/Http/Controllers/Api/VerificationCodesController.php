0a5083dd6a009ea0f1b6bea4ced1e340a671fe16
tongmeng <linzikristy@qq.com>
2020-01-17 14:41:13 +0000

    图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\VerificationCodeRequest;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
+use Mews\Captcha\Facades\Captcha;
 use Overtrue\EasySms\EasySms;
 use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
 
@@ -13,7 +15,19 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = Captcha::get($request->captcha_key);
+
+        if (!$captchaData) {
+            abort(403, '图片验证码已失效');
+        }
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            Cache::forget($request->captcha_key);
+            throw new AuthenticationException('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
         if (! app()->environment('production')) {
             $code = '1234';
         } else {
@@ -33,6 +47,8 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(5);
         // 缓存验证码 5 分钟过期。
         Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        Cache::forget($request->captcha_key);
 
         return response()->json([
             'key' => $key,
--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,16 @@ Route::prefix('v1')
                 // 用户注册
                 Route::post('users', 'UsersController@store')
                     ->name('users.store');
+
+                // 图片验证码
+                Route::post('captchas', 'CaptchasController@store')
+                    ->name('captchas.store');
+                // 短信验证码
+                Route::post('verificationCodes', 'VerificationCodesController@store')
+                    ->name('verificationCodes.store');
+                // 用户注册
+                Route::post('users', 'UsersController@store')
+                    ->name('users.store');
             });
 
         Route::middleware('throttle:' . config('api.rate_limits.access'))
