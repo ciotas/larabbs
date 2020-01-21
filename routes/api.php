1ed84fb823633f6035e622302bcd688ee12bdf5d
tongmeng <linzikristy@qq.com>
2020-01-21 11:05:40 +0000

    回复列表

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,12 @@ Route::prefix('v1')
                     ->name('categories.index');
                 // 话题列表，详情
                 Route::resource('topics', 'TopicsController')->only(['index', 'show']);
+                // 话题回复列表
+                Route::get('topics/{topic}/replies', 'RepliesController@index')
+                    ->name('topics.replies.index');
+                // 某个用户的回复列表
+                Route::get('users/{user}/replies', 'RepliesController@userIndex')
+                    ->name('users.replies.index');
                 // 分类类表
                 Route::get('categories', 'CategoriesController@index')->name('categories.index');
                 // 某个用户发布的话题
