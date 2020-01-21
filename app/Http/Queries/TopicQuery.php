769526427047bdfca59f525b8ef15c818829486c
tongmeng <linzikristy@qq.com>
2020-01-21 13:39:14 +0000

    显示用户角色

--- app/Http/Queries/TopicQuery.php
+++ app/Http/Queries/TopicQuery.php
@@ -10,7 +10,7 @@ class TopicQuery extends QueryBuilder
     public function __construct()
     {
         parent::__construct(Topic::query());
-        $this->allowedIncludes('user', 'category')
+        $this->allowedIncludes('user', 'category', 'user.roles')
             ->allowedFilters([
                 'title',
                 AllowedFilter::exact('category_id'),
