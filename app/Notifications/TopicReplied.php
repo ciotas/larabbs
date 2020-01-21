56a77ab4c0c1b7240bbe4fad442c447d4d733b4b
tongmeng <linzikristy@qq.com>
2020-01-21 15:11:45 +0000

    push notification

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -3,10 +3,12 @@
 namespace App\Notifications;
 
 use App\Models\Reply;
+use App\Notifications\Channels\JPushChannel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use JPush\PushPayload;
 
 class TopicReplied extends Notification implements ShouldQueue
 {
@@ -33,7 +35,7 @@ class TopicReplied extends Notification implements ShouldQueue
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database', 'mail'];
+        return ['database', 'mail', JPushChannel::class];
     }
 
     /**
@@ -70,6 +72,14 @@ class TopicReplied extends Notification implements ShouldQueue
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toJPush($notifiable, PushPayload $payload): PushPayload
+    {
+        return $payload
+            ->setPlatform('all')
+            ->addRegistrationId($notifiable->registration_id)
+            ->setNotificationAlert(strip_tags($this->reply->content));
     }
     /**
      * Get the array representation of the notification.
